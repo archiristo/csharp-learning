a88944a31508d5edb4e8094106abfc399b83266d
iris <109429124+archiristo@users.noreply.github.com>
2024-12-21 12:45:55 +0300

    Create love.cs

---
+++ love.cs
@@ -0,0 +1,8 @@
+string name;
+Console.Write("Name: ");
+name = Console.ReadLine();
+string beloved;
+Console.Write("What/Who do you love :3 ");
+beloved = Console.ReadLine();
+Console.WriteLine(":3");
+Console.WriteLine(name + " loves " + beloved + " <3");
